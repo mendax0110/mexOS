c6026542cddebbbd00a8931e1d9f44df4d103144
mendax3301 <adrian.goessl@outlook.com>
2026-08-08 16:52:27 +0200

    [mexOS][kernel][mm][arch] added handling for double faults, fixed memory issues, temporarly kernel panic added for spli_block in heap...
    Signed-off-by: mendax3301 <adrian.goessl@outlook.com>

--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -67,7 +67,8 @@ static int bitmap_first_free_s(const uint32_t size)
                     uint32_t free = 0;
                     for (uint32_t k = 0; k < size; k++)
                     {
-                        if (!bitmap_test(start + k)) { free++; }
+                        if (!bitmap_test(start + k)){ break; }
+                        free++;
                         if (free == size) { return (int)start; }
                     }
                 }
